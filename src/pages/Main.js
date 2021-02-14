e09b5bca7a71a03606528238f1e06099d961d86f
Jonathan Lee <jonathan.wing.lee@gmail.com>
2021-02-14 17:26:25 -0600

    Update main page

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,12 +1,24 @@
 import React, { useReducer } from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import LinearProgress from '@material-ui/core/LinearProgress';
+
+import Notes from '../components/Notes';
 
 const initialState = {
   home: [],
   work: [],
   personal: [],
 };
+
+/*
+    structure of a note: 
+        description
+        title 
+        category 
+        date
+        completed
+*/
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -17,6 +29,7 @@ const reducer = (state, action) => {
 
 const Main = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <section>
       <TextField />
@@ -25,8 +38,13 @@ const Main = () => {
         <Button>Home</Button>
         <Button>Work</Button>
         <Button>Personal</Button>
+        <Button>+ ADD NOTE</Button>
       </div>
-      <Button>+ ADD NOTE</Button>
+      <LinearProgress variant='determinate' value={0} />
+      <h3>You have {`${completedNotes} / ${totalNotes}`} notes completed</h3>
+      <Notes />
+      {/* {if there are no notes, show the 'you donnt have any notes'} */}
+      {/* {if there}  */}
     </section>
   );
 };
