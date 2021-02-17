916acc80111020210c82696cd525a10fd08a9f97
Jonathan Lee <jonathan.wing.lee@gmail.com>
2021-02-16 21:35:09 -0600

    Setup main page

---
+++ src/components/Search.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import InputBase from '@material-ui/core/InputBase';
+import IconButton from '@material-ui/core/IconButton';
+import SearchIcon from '@material-ui/icons/Search';
+import Box from '@material-ui/core/Box';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: '2px 4px',
+    display: 'flex',
+    alignItems: 'center',
+    width: '68%',
+    backgroundColor: 'white',
+  },
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 1,
+  },
+  iconButton: {
+    padding: 10,
+  },
+  divider: {
+    height: 28,
+    margin: 4,
+  },
+}));
+
+const CustomSearch = ({ name, onChange, value }) => {
+  const classes = useStyles();
+
+  return (
+    <Box component='form' boxShadow={2} className={classes.root}>
+      <IconButton
+        type='submit'
+        className={classes.iconButton}
+        aria-label='search'
+      >
+        <SearchIcon />
+      </IconButton>
+      <InputBase
+        className={classes.input}
+        placeholder='Search notes...'
+        inputProps={{ 'aria-label': 'search google maps' }}
+        onChange={onChange}
+        name={name}
+        value={value}
+      />
+    </Box>
+  );
+};
+
+export default CustomSearch;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,8 +2,12 @@ import React, { useReducer } from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import LinearProgress from '@material-ui/core/LinearProgress';
+import AddIcon from '@material-ui/icons/Add';
+import Search from '../components/Search';
+import styled from 'styled-components';
 
 import Notes from '../components/Notes';
+import { ReactComponent as EmptyNotes } from '../assets/add-note-illustration.svg';
 
 const initialState = {
   home: [],
@@ -11,13 +15,35 @@ const initialState = {
   personal: [],
 };
 
+// styled-components
+const ActionsContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+
+const EmptyNotesMessage = styled.h1`
+  color: gray;
+  padding: 1rem;
+`;
+
+const FiltersContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const SearchContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 3rem 1rem 1rem;
+`;
+
 /*
-    structure of a note: 
-        description
-        title 
-        category 
-        date
-        completed
+  structure of a note: 
+      description
+      title 
+      category 
+      date
+      completed
 */
 
 const reducer = (state, action) => {
@@ -29,22 +55,41 @@ const reducer = (state, action) => {
 
 const Main = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
-
+  const { completedNotes, totalNotes } = state;
   return (
     <section>
-      <TextField />
-      <div>
-        <Button>All</Button>
-        <Button>Home</Button>
-        <Button>Work</Button>
-        <Button>Personal</Button>
-        <Button>+ ADD NOTE</Button>
-      </div>
-      <LinearProgress variant='determinate' value={0} />
-      <h3>You have {`${completedNotes} / ${totalNotes}`} notes completed</h3>
-      <Notes />
+      <SearchContainer>
+        <Search />
+      </SearchContainer>
+      <ActionsContainer>
+        <FiltersContainer>
+          <Button variant='contained' color='primary'>
+            All
+          </Button>
+          <div>
+            <Button>Home</Button>
+          </div>
+          <div>
+            <Button>Work</Button>
+          </div>
+          <div>
+            <Button>Personal</Button>
+          </div>
+        </FiltersContainer>
+        <Button variant='contained' color='primary'>
+          <AddIcon />
+          &nbsp;ADD NOTE
+        </Button>
+      </ActionsContainer>
+      {/* <LinearProgress variant='determinate' value={0} />
+      <h3>
+        You have {`${completedNotes || 0} / ${totalNotes || 1}`} notes completed
+      </h3>
+      <Notes /> */}
       {/* {if there are no notes, show the 'you donnt have any notes'} */}
       {/* {if there}  */}
+      <EmptyNotesMessage>You don't have any notes</EmptyNotesMessage>
+      <EmptyNotes />
     </section>
   );
 };
